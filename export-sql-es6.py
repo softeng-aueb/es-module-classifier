24710c89a22fc519c1d98cf28b6b55edf3f94b82
Vassilis <bzafiris@gmail.com>
2021-05-24 14:42:09 +0000

    Included line starts in regexes

--- export-sql-es6.py
+++ export-sql-es6.py
@@ -4,23 +4,26 @@ def regex_contains(exp, shouldMatch):
     return f" REGEXP_CONTAINS(content, r'{exp}') = {'true' if shouldMatch else 'false'} "
 
 def getBigQueryStmts(regexes, shouldMatch, shouldMatchAny):
-    bq_expr = map(lambda exp: regex_contains(exp, shouldMatch), regexes)
-    if shouldMatchAny:
-        return " OR \n".join(bq_expr)
-    else:
-        return " AND \n".join(bq_expr)
+    stmt = ""
+    sep = " OR " if shouldMatchAny else " AND "
+    bq_exprs = map(lambda exp: regex_contains(exp, shouldMatch), regexes)
+    # try to combine \n in literal or print form
+    stmt = repr(sep.join(bq_exprs)).replace("\\\\", "\\")
+    list = stmt.split("OR")
+    stmt = (sep + "\n").join(list)
+    return stmt
 
 print('---------------- ES6 conditions ------------------------- ')
 
 bq_stmt = getBigQueryStmts(regexes_import + regexes_export, True, True)
-print(repr(bq_stmt))
+print(bq_stmt)
 
 print('----------------- AMD conditions --------------------------')
 bq_stmt = getBigQueryStmts(regexes_amd, True, True)
-print(repr(bq_stmt))
+print(bq_stmt)
 
 print('----------------- CJS conditions --------------------------')
 bq_stmt = getBigQueryStmts(regexes_cjs, True, True)
-print(repr(bq_stmt))
+print(bq_stmt)
 
     
